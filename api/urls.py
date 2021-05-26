f20bedcc08df7f345e8646e9e2d03c939281fe9c
rrojan20 <rrojan20@gmail.com>
2021-05-26 15:48:29 +0545

    added get-token route to get user token

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from questions import api_views as questions
 from rest_framework.routers import SimpleRouter
 
+from rest_framework.authtoken import views as authtoken
+
+
 router = SimpleRouter()
 router.register(r'questions', questions.QuestionList, basename="questions_list")
 router.register(r'submit', questions.SubmissionViewSet, basename="submit")
@@ -9,5 +12,6 @@ router.register(r'submit', questions.SubmissionViewSet, basename="submit")
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
+    path('get-token/', authtoken.obtain_auth_token),
     path('', include(router.urls)),
 ]
