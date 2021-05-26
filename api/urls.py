f88c928337e2fe5fec996625dc469ccccb8ba86f
rrojan20 <rrojan20@gmail.com>
2021-05-26 16:44:40 +0545

    added feature: signup

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path, include
 from questions import api_views as questions
 from rest_framework.routers import SimpleRouter
-
 from rest_framework.authtoken import views as authtoken
+from users import api_views as users
 
 
 router = SimpleRouter()
-router.register(r'questions', questions.QuestionList, basename="questions_list")
+router.register(r'questions', questions.QuestionList,
+                basename="questions_list")
 router.register(r'submit', questions.SubmissionViewSet, basename="submit")
 
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('get-token/', authtoken.obtain_auth_token),
+    path('get-token/', authtoken.obtain_auth_token, name='get_token'),
+    path('register/', users.Register.as_view(), name='register'),
     path('', include(router.urls)),
 ]
---
+++ users/serializers.py
@@ -0,0 +1,90 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = (
+            'name',
+            'phone_no',
+            'hospital_name',
+            'hospital_ward_name',
+            'address',
+            'province',
+            'occupation',
+            'sex',
+            'age',
+            'day_of_pcr_positive',
+            'PCR_CT_value',
+            'vaccination_status',
+            'prevalent_conditions',
+            'stage_of_patient',
+            'comorbidity_problems',
+            'smoking_status',
+        )
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(required=True)
+    password1 = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        validators=[validate_password],
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'profile')
+
+    def validate(self, attrs):
+        if attrs['password1'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+        )
+        user.set_password(validated_data['password1'])
+
+        profile_data = validated_data.pop('profile')
+        
+        try:
+            obj = Profile.objects.get(user=user)
+            obj.delete()
+        except:
+            pass
+
+        profile = Profile.objects.create(
+            user = user,
+            name = profile_data['name'],
+            phone_no = profile_data['phone_no'],
+            hospital_name = profile_data['hospital_name'],
+            hospital_ward_name = profile_data['hospital_ward_name'],
+            address = profile_data['address'],
+            province = profile_data['province'],
+            occupation = profile_data['occupation'],
+            sex = profile_data['sex'],
+            age = profile_data['age'],
+            day_of_pcr_positive = profile_data['day_of_pcr_positive'],
+            PCR_CT_value = profile_data['PCR_CT_value'],
+            vaccination_status = profile_data['vaccination_status'],
+            prevalent_conditions = profile_data['prevalent_conditions'],
+            stage_of_patient = profile_data['stage_of_patient'],
+            comorbidity_problems = profile_data['comorbidity_problems'],
+            smoking_status = profile_data['smoking_status'],
+        )
+        user.save()
+        profile.save()
+
+        return user
